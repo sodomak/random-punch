32f737dc54835c2cf089264c106fdcb496ecb4a5
sodomak <cancel@cancel.cz>
2024-12-05 11:52:53 +0100

    feat: update bump_version script to sync Android version

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -9,9 +9,18 @@ fi
 # Run the Dart script to update versions
 dart tools/update_version.dart "$1"
 
-# If successful, commit and push
+# If successful, update Android version and commit
 if [ $? -eq 0 ]; then
-    git add lib/src/version.dart pubspec.yaml
+    # Get the version code from pubspec.yaml (number after +)
+    VERSION_CODE=$(grep "version:" pubspec.yaml | sed 's/.*+//')
+    
+    # Update Android version in build.gradle using sed
+    sed -i'' -e "s/versionCode .*/versionCode $VERSION_CODE/" \
+             -e "s/versionName .*/versionName \"$1\"/" \
+             android/app/build.gradle
+    
+    # Commit and push all version-related changes
+    git add lib/src/version.dart pubspec.yaml android/app/build.gradle
     git commit -m "Bump version to $1"
     git push origin main
 fi 
